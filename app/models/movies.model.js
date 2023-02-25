4a57f6d50438f59d44beb5947262f9402961f881
Kobie127 <arndtkobie@gmail.com>
2023-02-25 16:46:21 -0500

    addition

--- app/models/movies.model.js
+++ app/models/movies.model.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, Sequelize) => {
             type: Sequelize.STRING
         },
         release: {
-            type: Sequelize.DATETIME
+            type: Sequelize.DATE
         },
         director: {
             type: Sequelize.STRING
